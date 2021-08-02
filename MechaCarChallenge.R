eb2bb6339eb958b1bba264d50d62f7739d20a7dd
naomishields <58480113+naomishields@users.noreply.github.com>
2021-08-02 16:20:39 -0400

    summary statistics on suspension coils

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,19 @@
+## Deliverable One ##
 #loading dplyr
 library(dplyr)
 #importing data
 mechaCar_mpg <- read.csv(file = 'data/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+#performing linear regression
+model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + factor(AWD),
+            data = mechaCar_mpg)
+#summary of model
+summary(model)
+
+## Deliverable Two ##
+#importing data 
+suspension_coil <- read.csv(file = 'data/Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+#creating summary table of PSI 
+total_summary <- suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+#creating summary table of PSI by lot
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot)%>% summarize(Mean=mean(PSI),Median=median(PSI),
+                                                                            Variance=var(PSI),SD=sd(PSI),.groups = 'keep')
