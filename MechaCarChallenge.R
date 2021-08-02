0206f5f69dbf5733395757c2fe2ad0f94a46b512
naomishields <58480113+naomishields@users.noreply.github.com>
2021-08-02 16:59:48 -0400

    t-tests on suspension coils

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -17,3 +17,17 @@ total_summary <- suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI)
 #creating summary table of PSI by lot
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot)%>% summarize(Mean=mean(PSI),Median=median(PSI),
                                                                             Variance=var(PSI),SD=sd(PSI),.groups = 'keep')
+## Deliverable Three ##
+# t test for all lots
+t.test(suspension_coil$PSI, mu= 1500)
+#making lots a numeric factor 
+suspension_coil$Manufacturing_Lot <- as.numeric(factor(suspension_coil$Manufacturing_Lot))
+# t test for lot 1 
+lot1 <- subset(suspension_coil, Manufacturing_Lot == 1 , select = c(Manufacturing_Lot,PSI))
+t.test(lot1$PSI, mu= 1500)
+#t test for lot 2
+lot2 <- subset(suspension_coil, Manufacturing_Lot == 2 , select = c(Manufacturing_Lot,PSI))
+t.test(lot2$PSI, mu= 1500)
+#t test for lot 3
+lot3 <- subset(suspension_coil, Manufacturing_Lot == 3 , select = c(Manufacturing_Lot,PSI))
+t.test(lot3$PSI, mu= 1500)
